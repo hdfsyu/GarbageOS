9440354820417f87e1487f80f48b1f861c09b1f6
Viyaan Chauhan <viyaanchauhan@hotmail.com>
2022-09-24 09:20:44 -0400

    Update main.c

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,29 +1,11 @@
-#include <stdint.h>
 #include "print.h"
 
-#if UINT32_MAX == UINTPTR_MAX
-#define STACK_CHK_GUARD 0xe2dae263
-#else
-#define STACK_CHK_GUARD 0x599e9fbd94fdf737
-#endif
- 
-uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
- 
- // Stack Smahing Check Failure Handler
-__attribute__((noreturn))
-void __stack_chk_fail(void)
-{
-#if __STDC_HOSTED__
-	abort();
-#elif __is_myos_kernel
-	panic("Panic #0");
-#endif
-}
-// Starts the kernel's main printing process.
-void kernel_main(){
+void kernel_main() {
     print_clear();
-    print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_GREEN);// First declare the background of the text, then the color of the text itself. Refer to print.h for a list of colours.
-    print_str("OK\n");
-    print_set_color(PRINT_COLOR_MAGENTA, PRINT_COLOR_BLUE);
-    print_str("Welcome to VynOS! (work in progress.)");
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("Welcome to GarbageOS!\n");
+    print_set_color(PRINT_COLOR_BLUE, PRINT_COLOR_BLACK);
+    print_str("Not very good is it?\n");
+    print_set_color(PRINT_COLOR_BROWN, PRINT_COLOR_BLACK);
+    print_str("It is called 'GarbageOS' So yea...\n");
 }
